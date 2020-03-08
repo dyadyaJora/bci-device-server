cb57b6e32348e2b4a5f2a454c247b7e041b96901
dyadyaJora <ja_massa97@mail.ru>
2020-03-08 13:33:24 +0300

    implement vr scripts

---
+++ device-scripts/simple.js
@@ -0,0 +1,21 @@
+var SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline')
+var port = new SerialPort('/dev/ttyACM0', {baudRate: 9600});
+
+
+console.log("1: ");
+
+// Pipe the data into another stream (like a parser or standard out)
+const lineStream = port.pipe(new Readline())
+
+var interval;
+lineStream.on('data', line => {
+	let date = new Date();
+	let strRes = line.toString() + ' ' + date.toISOString();
+	debugger;
+	console.log(line); console.log(strRes);
+	clearInterval(interval)
+})
+
+interval = setInterval( () =>
+port.write('R', (err, data) => {console.log(err, data)}), 1000);
