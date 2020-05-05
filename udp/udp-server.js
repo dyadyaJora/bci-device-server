f040771c408676b0038a030769b9209b80abbd31
dyadyaJora <ja_massa97@mail.ru>
2020-05-05 23:57:57 +0300

    update udp server

--- udp/udp-server.js
+++ udp/udp-server.js
@@ -1,4 +1,5 @@
 const dgram = require('dgram');
+const rp = require('request-promise');
 const server = dgram.createSocket('udp4');
 const Influx = require('influx');
 const INFLUX_HOST = 'localhost';
@@ -7,6 +8,10 @@ const influx = new Influx.InfluxDB({
     host: INFLUX_HOST,
     database: DB_NAME
 });
+const MEMCACHED_HOST = '127.0.0.1';
+const MEMCACHED_PORT = '12346';
+const Memcached = require('memcached');
+const memcached = new Memcached(MEMCACHED_HOST + ':' + MEMCACHED_PORT);
 
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
@@ -14,11 +19,32 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
     let message = JSON.parse(msg);
-    if (message.type && message.type === 'bandPower') {
-        saveBandPower(message);
+
+    if (!(message.type && message.type === 'bandPower')) {
+        return;
     }
+
+    validateMessage(message)
+        .then(exists => {
+            if (exists) {
+                return saveBandPower(message);
+            }
+
+            let deviceSessionUrl = 'http://127.0.0.1:3001/api/v1/device/' + message.deviceId + '/session/' + message.sessionId;
+
+            return rp.get(deviceSessionUrl)
+                .then(data => {
+                    console.log(data);
+                    return new Promise((resolve) => resolve())
+                });
+        })
+        .then((data) => {
+            console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+        })
+        .catch(err => {
+            console.error(err);
+        })
 });
 
 server.on('listening', () => {
@@ -43,9 +69,27 @@ function saveBandPower(message) {
         },
         timestamp: +(new Date()) + "000000"
     }
-    influx.writePoints([point])
-        .then((data) => {
-            console.log("Data saved!")
-        });
+    return influx.writePoints([point])
+}
 
+function validateMessage(message) {
+    let deviceHash = message.deviceId;
+    let sessionHash = message.sessionId;
+
+    return new Promise((resolve, reject) => {
+        memcached.get(sessionHash, function(err, data) {
+           if (!!err) {
+               reject(err);
+               return;
+           }
+
+           if (!data) {
+               resolve(false);
+               return;
+           }
+
+           let exists = data.client.hash === deviceHash;
+           resolve(exists);
+        });
+    });
 }
